b9b2e38b7bfbe8646418444c9685ba89d7dcd8e3
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-02-17 17:14:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -154,6 +154,28 @@ print('\nLoad Dataset')
 
 if args.task == 'task_1_tumor_vs_normal':
     args.n_classes=2
+    dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/tumor_vs_normal_dummy_clean.csv',
+                            data_dir= os.path.join(args.data_root_dir, 'tumor_vs_normal_resnet_features'),
+                            shuffle = False, 
+                            seed = args.seed, 
+                            print_info = True,
+                            label_dict = {'normal_tissue':0, 'tumor_tissue':1},
+                            patient_strat=False,
+                            ignore=[])
+
+elif args.task == 'task_2_tumor_subtyping':
+    args.n_classes=3
+    dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/tumor_subtyping_dummy_clean.csv',
+                            data_dir= os.path.join(args.data_root_dir, 'tumor_subtyping_resnet_features'),
+                            shuffle = False, 
+                            seed = args.seed, 
+                            print_info = True,
+                            label_dict = {'subtype_1':0, 'subtype_2':1, 'subtype_3':2},
+                            patient_strat= False,
+                            ignore=[])
+
+if args.task == 'task_3_prog_vs_noprog':
+    args.n_classes=2
     dataset = Generic_MIL_Dataset(csv_path = '/home/jupyter/her2low_cohort_for_clam.csv',
                             data_dir= args.data_root_dir,
                             shuffle = False, 
@@ -162,18 +184,7 @@ if args.task == 'task_1_tumor_vs_normal':
                             label_dict = {'no_progression':0, 'progression':1},
                             patient_strat=False,
                             ignore=[])
-
-elif args.task == 'task_2_tumor_subtyping':
-    args.n_classes=3
-    dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/tumor_subtyping_dummy_clean.csv',
-                            data_dir= args.data_root_dir,
-                            shuffle = False, 
-                            seed = args.seed, 
-                            print_info = True,
-                            label_dict = {'subtype_1':0, 'subtype_2':1, 'subtype_3':2},
-                            patient_strat= False,
-                            ignore=[])
-
+    
     if args.model_type in ['clam_sb', 'clam_mb']:
         assert args.subtyping 
         
