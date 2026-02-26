5e4664a2e4d10ce0e4f560ade313c9c0cea2c1c5
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-02-26 13:46:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,10 @@ parser.add_argument('--model_type', type=str, choices=['clam_sb', 'clam_mb', 'mi
 parser.add_argument('--exp_code', type=str, help='experiment code for saving results')
 parser.add_argument('--weighted_sample', action='store_true', default=False, help='enable weighted sampling')
 parser.add_argument('--model_size', type=str, choices=['small', 'big'], default='small', help='size of model, does not affect mil')
-parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'task_3_prog_vs_noprog'])
+parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'task_3_prog_vs_noprog', 'task_4_survival_binned_ce'])
+parser.add_argument('--survival_csv', type=str,
+                    default='/home/jupyter/her2low_project/her2low_survival_cohort.csv',
+                    help='path to survival cohort CSV')
 ### CLAM specific options
 parser.add_argument('--no_inst_cluster', action='store_true', default=False,
                      help='disable instance-level clustering')
@@ -184,6 +187,19 @@ elif args.task == 'task_3_prog_vs_noprog':
                             label_dict = {'no_progression':0, 'progression':1},
                             patient_strat=False,
                             ignore=[]) 
+
+elif args.task == 'task_4_survival_binned':
+    args.n_classes = 4
+    dataset = Generic_MIL_Dataset(
+        csv_path      = args.survival_csv,
+        data_dir      = args.data_root_dir,
+        shuffle       = False,
+        seed          = args.seed,
+        print_info    = True,
+        label_dict    = {0: 0, 1: 1, 2: 2, 3: 3},
+        patient_strat = False,
+        ignore        = []
+    )
         
 else:
     raise NotImplementedError
