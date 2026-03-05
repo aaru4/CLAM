8bc94a5b77f06246d468d4561ddc5b525aea054c
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-03-05 15:55:01 -0500

    Update create_splits_seq.py

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -12,7 +12,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed (default: 1)')
 parser.add_argument('--k', type=int, default=10,
                     help='number of splits (default: 10)')
-parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'task_3_prog_vs_noprog'])
+parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'task_3_prog_vs_noprog', 'task_4_survival_binned_ce', 'task_5_survival_nll', 'task_6_survival_cox'])
 parser.add_argument('--val_frac', type=float, default= 0.1,
                     help='fraction of labels for validation (default: 0.1)')
 parser.add_argument('--test_frac', type=float, default= 0.1,
