34b8b9fda082daa1f4c45ef6ea8e2cf7a8b45f12
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-03-06 15:09:50 -0500

    Update create_splits_seq.py

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -56,7 +56,7 @@ elif args.task == 'task_3_prog_vs_noprog':
 elif args.task in ['task_4_survival_binned_ce', 'task_5_survival_nll']:
     args.n_classes = 4
     dataset = Generic_WSI_Classification_Dataset(
-        csv_path      = '/home/jupyter/her2low_project/her2low_survival_cohort.csv',
+        csv_path      = '/home/jupyter/her2low_project/aaru_task4_survival_cohort.csv',
         shuffle       = False,
         seed          = args.seed,
         print_info    = True,
@@ -68,7 +68,7 @@ elif args.task in ['task_4_survival_binned_ce', 'task_5_survival_nll']:
 elif args.task == 'task_6_survival_cox':
     args.n_classes = 1
     dataset = Generic_WSI_Classification_Dataset(
-        csv_path      = '/home/jupyter/her2low_project/her2low_survival_cohort.csv',
+        csv_path      = '/home/jupyter/her2low_project/aaru_task4_survival_cohort.csv',
         shuffle       = False,
         seed          = args.seed,
         print_info    = True,
