a4d3250def059cfe3f3e19071bffb0c6943bc2ac
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-04-05 09:20:40 -0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -53,26 +53,21 @@ def get_simple_loader(dataset, batch_size=1, num_workers=1):
 	loader = DataLoader(dataset, batch_size=batch_size, sampler = sampler.SequentialSampler(dataset), collate_fn = collate_MIL, **kwargs)
 	return loader 
 
-def get_split_loader(split_dataset, training = False, testing = False, weighted = False):
-	"""
-		return either the validation loader or training loader 
-	"""
-	kwargs = {'num_workers': 4} if device.type == "cuda" else {}
-	if not testing:
-		if training:
-			if weighted:
-				weights = make_weights_for_balanced_classes_split(split_dataset)
-				loader = DataLoader(split_dataset, batch_size=1, sampler = WeightedRandomSampler(weights, len(weights)), collate_fn = collate_MIL, **kwargs)	
-			else:
-				loader = DataLoader(split_dataset, batch_size=1, sampler = RandomSampler(split_dataset), collate_fn = collate_MIL, **kwargs)
-		else:
-			loader = DataLoader(split_dataset, batch_size=1, sampler = SequentialSampler(split_dataset), collate_fn = collate_MIL, **kwargs)
-	
-	else:
-		ids = np.random.choice(np.arange(len(split_dataset), int(len(split_dataset)*0.1)), replace = False)
-		loader = DataLoader(split_dataset, batch_size=1, sampler = SubsetSequentialSampler(ids), collate_fn = collate_MIL, **kwargs )
-
-	return loader
+def get_split_loader(split_dataset, training=False, testing=False, weighted=False, collate_fn=collate_MIL):
+    kwargs = {'num_workers': 4} if device.type == "cuda" else {}
+    if not testing:
+        if training:
+            if weighted:
+                weights = make_weights_for_balanced_classes_split(split_dataset)
+                loader = DataLoader(split_dataset, batch_size=1, sampler=WeightedRandomSampler(weights, len(weights)), collate_fn=collate_fn, **kwargs)
+            else:
+                loader = DataLoader(split_dataset, batch_size=1, sampler=RandomSampler(split_dataset), collate_fn=collate_fn, **kwargs)
+        else:
+            loader = DataLoader(split_dataset, batch_size=1, sampler=SequentialSampler(split_dataset), collate_fn=collate_fn, **kwargs)
+    else:
+        ids = np.random.choice(np.arange(len(split_dataset), int(len(split_dataset)*0.1)), replace=False)
+        loader = DataLoader(split_dataset, batch_size=1, sampler=SubsetSequentialSampler(ids), collate_fn=collate_fn, **kwargs)
+    return loader
 
 def get_optim(model, args):
 	if args.opt == "adam":
