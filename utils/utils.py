324447946693024f237e760b052ee7198a0861ec
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-04-09 11:40:05 -0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -49,12 +49,12 @@ def collate_features(batch):
 
 
 def get_simple_loader(dataset, batch_size=1, num_workers=1):
-	kwargs = {'num_workers': 4, 'pin_memory': False, 'num_workers': num_workers} if device.type == "cuda" else {}
+	kwargs = {'num_workers': 0, 'pin_memory': False, 'num_workers': num_workers} if device.type == "cuda" else {}
 	loader = DataLoader(dataset, batch_size=batch_size, sampler = sampler.SequentialSampler(dataset), collate_fn = collate_MIL, **kwargs)
 	return loader 
 
 def get_split_loader(split_dataset, training=False, testing=False, weighted=False, collate_fn=collate_MIL):
-    kwargs = {'num_workers': 4} if device.type == "cuda" else {}
+    kwargs = {'num_workers': 0} if device.type == "cuda" else {}
     if not testing:
         if training:
             if weighted:
